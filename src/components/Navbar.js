35b2790182755d4040155c4e1fbe0911b634cb2f
i-ice-bear <andydowsen47us@gmail.com>
2023-05-26 12:36:17 +0530

    Lecture 7 Navbar Initialised from proptypes

---
+++ src/components/Navbar.js
@@ -0,0 +1,56 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import PropTypes from "prop-types";
+
+const NavbarComponent = (props) => {
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" className="p-3">
+      <Container>
+        <div className="imageContainer w-12 h-12 mx-3">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png"
+            className="animate-spin"
+            alt="React logo"
+          />
+        </div>
+        <Navbar.Brand href="#home">{props.heading}</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#deets">More deets</Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+              Dank memes
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavbarComponent;
+
+NavbarComponent.propTypes = {
+  heading: PropTypes.string.isRequired,
+};
+
+NavbarComponent.defaultProps = {
+    heading: "Enter your title here"
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
+"use strict";
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
